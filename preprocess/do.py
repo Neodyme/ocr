5c9c7835544a6b7140fe3740ffd20ed64f352b38
prost p <prost_p@epitech.eu>
2014-11-03 17:31:00 +0100

    ameliorant

--- preprocess/do.py
+++ preprocess/do.py
@@ -5,16 +5,14 @@ import cv2
 
 def threshold(img):
 
-    # Otsu's thresholding
-    ret1,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-    ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    blur = cv2.GaussianBlur(img,(5,5),0)
+    cv2.imshow('preprocess: before bw', img)
+    cv2.waitKey(0)
+    blur = cv2.GaussianBlur(img,(1,1),0)
     ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-                            
     cv2.imshow('preprocess: after bw', th3)
     cv2.waitKey(0)
-    return th2
+    return th3
 
 def do(filename):
     img = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
