486e133cfcbe2e4b297e657ea6242c7c445e56d7
Nevermethion <timothee.maurin@eptiech.eu>
2014-11-04 21:58:07 +0100

    add of the cropping to remove useless white part, and resize to fixed size

--- preprocess/do.py
+++ preprocess/do.py
@@ -4,6 +4,43 @@
 import cv2
 import numpy as np
 
+TARGET_WIDTH = 32
+TARGET_HEIGHT = 32
+
+#
+# Crop the letter to let a minimum of white on screen
+def crop(img):
+    height, width = img.shape
+
+    min_height = height
+    max_height = 0
+    min_width = width
+    max_width = 0
+
+    for i in range(0, height):
+        for j in range(0, width):
+            if img[i, j] == 0:
+                if j < min_height:
+                    min_height = j
+                if j > max_height:
+                    max_height = j
+                if i < min_width:
+                    min_width = i
+                if i > max_width:
+                    max_width = i
+    print(min_height, max_height, min_width, max_width)
+    img_crop = img[min_width:max_width, min_height:max_height]
+    cv2.imshow('preprocess: after crop', img_crop)
+    cv2.waitKey(0)
+
+    return img_crop
+
+#
+# Resize the image to a fixed dimension define by TARGET_WIDTH and TARGET_HEIGHT
+def resize(img):
+    img_resize = cv2.resize(img, (TARGET_WIDTH, TARGET_HEIGHT))
+    return img_resize
+
 #
 # 
 def erode(img):
@@ -33,6 +70,8 @@ def threshold(img):
 def do(filename):
     img = cv2.imread(filename, cv2.CV_LOAD_IMAGE_GRAYSCALE)
     img = threshold(img)
+    img = crop(img)
+    img = resize(img)
     img = erode(img)
     return img
     
