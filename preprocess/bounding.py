cba5becffb2ead94761668cd807b3c0c3bc24c36
Nevermethion <timothee.maurin@eptiech.eu>
2014-11-16 22:30:36 +0100

    amelioration de la fusion des mots pour le wordBounding

--- preprocess/bounding.py
+++ preprocess/bounding.py
@@ -22,7 +22,7 @@ def bounding_letter(img):
     letter = []
     retBox = []
     for i in range(0, len(contours) - 1):
-        print(hierarchy[0][i])
+#        print(hierarchy[0][i])
         if hierarchy[0][i][3] != 0:
             continue
 
@@ -31,11 +31,10 @@ def bounding_letter(img):
         retBox.append(box)
         cv2.rectangle(img2, (box[0], box[1]), (box[0] + box[2], box[1] + box[3]), (0,0,200),1)
         letter.append(img2[box[1]:(box[1] + box[3]), box[0]:(box[0] + box[2])])
-#        print(letter[len(letter) - 1])
-#        cv2.imshow('letter bounding detection', letter[len(letter) - 1])
-#        cv2.waitKey(0)
-
-    print(box)
+        #        print(letter[len(letter) - 1])
+        #        cv2.imshow('letter bounding detection', letter[len(letter) - 1])
+        #        cv2.waitKey(0)
+#    print(box)
     cv2.imshow('end letter bounding detection', img2)
     cv2.waitKey(0)
     return letter, retBox
@@ -64,7 +63,8 @@ def bounding_word(img):
             gr.append(n)
         for a in box:
             if (abs(b[0] - (a[0] + a[2])) < 5 or abs(a[0] - (b[0] + b[2])) < 5)\
-               and abs((a[1] + a[3] / 2) - b[1] + b[3] / 2) < 10:
+               and (b[1] <= a[1] + a[3] and b[1] >= a[1] or \
+                    b[1] + b[3] <= a[1] + a[3] and b[1] + b[3] >= a[1]):
                 n.append(a)
                 used.append(a)
         used.append(b)
@@ -116,7 +116,6 @@ def bounding_word(img):
         
             #         roi = img[y:y + h, x:x + w]
 #    print(gr)
-    cv2.imshow('word bounding detection', img2)
+    cv2.imshow('test word bounding detection', img2)
     cv2.waitKey(0)
     return img
-
