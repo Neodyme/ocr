7476ae3a5a8594b710e612adfc51ba390b1a1191
prost p <prost_p@epitech.eu>
2014-11-19 14:27:28 +0100

    moficiation nom d'appels dans scan

--- scan.py
+++ scan.py
@@ -2,7 +2,7 @@
 #-*- encoding: utf-8 -*-
 # 
 # Started on  Thu Oct 30 14:46:24 2014 Prost P.
-## Last update Tue Nov 11 21:45:24 2014 Prost P.
+## Last update Wed Nov 19 14:01:55 2014 Prost P.
 #
 
 import cv
@@ -11,11 +11,11 @@ import preprocess
 #
 # cycle de scan de caracteres uniques
 def scan(filename):
-    img = preprocess.do.do(filename)
+    img = preprocess.process_char(filename)
     return
 #
 # cycle de scan de text complet
 def scantext(filename):
 #    img = preprocess.bounding_word(preprocess.do2(filename))
-    img = preprocess.bounding_letter(preprocess.do2(filename))
+    img = preprocess.bounding_letter(preprocess.process_text(filename))
     return
